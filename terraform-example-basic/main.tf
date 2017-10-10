d37215c7783047070c3899bef347551f2b4ccd56
Ubuntu <ubuntu@ip-172-31-16-129.us-east-2.compute.internal>
2017-10-10 06:57:22 +0000

    adding initial terraform tutorial

---
+++ terraform-example-basic/main.tf
@@ -0,0 +1,15 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  # AMI ID for Amazon Linux AMI 2016.03.0 (HVM)
+  ami           = "ami-2fa0ba56"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "example"
+  }
+}
